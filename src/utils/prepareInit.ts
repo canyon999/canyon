6c239717b4626552b0559f4d4405ce64e03e795f
Allen Zhang (张涛) <tzhangm@trip.com>
2022-01-20 14:34:00 +0800

    upd

--- src/utils/prepareInit.ts
+++ src/utils/prepareInit.ts
@@ -6,7 +6,9 @@ export const prepareInit = () => {
   configLog({ type: 'main.ts', info: `当前环境为${process.env.CUSTOM_ENV}` })
   configLog({ type: 'main.ts', info: `当前版本为${'v1.2.3'}` })
   const conf = fs.readFileSync(
-    `./conf/application${process.env.CUSTOM_ENV}.yml`,
+    `./conf/application${
+      process.env.CUSTOM_ENV ? process.env.CUSTOM_ENV : ''
+    }.yml`,
     'utf8',
   )
   global.conf = YAML.parse(conf)
