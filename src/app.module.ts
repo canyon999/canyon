fce5c04a9875ed98c22b2333a53eea832c0f6e29
zhangtao25 <tzhangm@trip.com>
2022-05-01 09:26:03 +0800

    添加v2.0.1 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './apps/auth/auth.module'
-
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
